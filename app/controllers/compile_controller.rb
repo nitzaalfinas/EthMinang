fd414447691b463a5c6732ae56a31274f6d18602
Nitza Alfinas <nitzaalfinas@gmail.com>
2016-09-21 15:44:10 +0700

    compile

--- app/controllers/compile_controller.rb
+++ app/controllers/compile_controller.rb
@@ -1,3 +1,12 @@
-<div class="container">
-    <textarea></textarea>
-</div>
+class CompileController < ApplicationController
+
+    def index
+    end
+
+    def this_code
+        @run = params[:the_name]
+        @rpc = %x[#{@run}]
+        puts @rpc
+        render inline: @rpc
+    end
+end
